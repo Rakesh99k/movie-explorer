5017a2c614ed21f739a561394be9d072acbda1c8
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2025-07-03 16:58:39 +0530

    modified Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 
 function Navbar(){
     return(
         <nav style={{backgroundColor: '#0a0a23', padding: '1rem'}}>
-            <hi style={{color:'white'}}>🎥 Movie Explorer</hi>
+            <nav style={{ backgroundColor: '#0a0a23', padding: '1rem', display: 'flex', gap: '1rem' }}>
+  <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>🎥 Movie Explorer</Link>
+  <Link to="/watchlist" style={{ color: 'white', textDecoration: 'none' }}>📺 Watchlist</Link>
+</nav>
+
         </nav>
     );
 }
