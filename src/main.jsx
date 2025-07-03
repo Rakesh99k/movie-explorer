10ef1ff933ec0d38fd476e534e7d59676a7f703b
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2025-07-03 16:44:05 +0530

    modified main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { WatchlistProvider } from './context/WatchlistContext';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+ <WatchlistProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+</WatchlistProvider>
 );
