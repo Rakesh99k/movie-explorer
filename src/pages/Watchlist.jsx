e0cee74113d6c28098f318bb8eddc65a786bde1b
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2025-07-03 16:51:12 +0530

    added Watchlist.jsx

---
+++ src/pages/Watchlist.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { WatchlistContext } from '../context/WatchlistContext';
+import MovieCard from '../components/MovieCard';
+
+function Watchlist() {
+  const { watchlist } = useContext(WatchlistContext);
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>📺 Your Watchlist</h2>
+      {watchlist.length === 0 ? (
+        <p>No movies added yet!</p>
+      ) : (
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {watchlist.map(movie => (
+            <MovieCard
+              key={movie.id}
+              title={movie.title}
+              poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              date={movie.release_date}
+              id={movie.id}
+              isAdded={true}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Watchlist;
